33e8017944ca8762bb4ad535bc59206fbdd03c97
Jan Tielens <jtielens@microsoft.com>
2017-09-15 14:05:10 +0200

    "show notifications" now replying with all notifications in 1 msg

--- messages/dialogs/notifications-show.js
+++ messages/dialogs/notifications-show.js
@@ -6,21 +6,30 @@ const notUtils = require('./notifications-utils.js');
 module.exports = [
     function (session) {
         notUtils.retrieveNotifications(session.message.address.channelId, session.message.address.user.id).then(function (results) {
-            if (results.length > 0)
+            if (results.length > 0) {
+                var msg = '';
                 for (var result of results) {
+                    if(msg.length > 0)
+                        msg = msg + '\n';
+
                     switch (result.notificationtype) {
                         case "condition":
-                            session.send(`${result.symbol} ${result.operator} ${result.price} - ${result.name}`);
+                            msg = `${msg}- ${result.symbol} ${result.operator} ${result.price} - ${result.name}`;
+                            //session.send(`${result.symbol} ${result.operator} ${result.price} - ${result.name}`);
                             break;
                         case "interval":
-                            session.send(`${result.symbol} interval ${result.interval} (prev. price ${result.previousprice}) - ${result.name}`);
+                            //session.send(`${result.symbol} interval ${result.interval} (prev. price ${result.previousprice}) - ${result.name}`);
+                            msg = `- ${msg}- ${result.symbol} interval ${result.interval} (prev. price ${result.previousprice}) - ${result.name}`;
                             break;
                         default:
-                            session.send(`${result.symbol} unkown type: ` + result.notificationtype);
+                            //session.send(`${result.symbol} unkown type: ` + result.notificationtype);
+                            msg = `- ${msg}- ${result.symbol} unkown type: ` + result.notificationtype;
                             break;
                     }
 
                 }
+                session.send(msg);
+            }
             else
                 session.send('You don\'t have any notifications.');
             session.endDialog();
